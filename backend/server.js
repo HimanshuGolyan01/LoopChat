0d3120e51c41c2f72626092a9a1e3512ac342aeb
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2025-01-28 17:54:06 +0530

    Signup and login functionality

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
-const express = require("express")
-const env = require("dotenv")
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./Routes/userRouter"); 
+
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+app.use(express.json());
 
+app.use("/api/user", userRoutes); // Use the imported router
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000 , console.log(`The server is listening on PORT ${PORT}`))
+app.listen(PORT, console.log(`The server is listening on PORT ${PORT}`));
